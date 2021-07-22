89f25c67f288cce6e6cc9ce24c131815368ab366
shz-code <web.shahidul.alam@gmail.com>
2021-07-22 21:37:32 +0600

    Initial

---
+++ js/main.js
@@ -0,0 +1,73 @@
+
+
+window.onload = function () {
+
+    
+    let main = document.getElementById('main');
+    let pre = document.getElementById('loader');
+    
+    // main.style.display = 'block' ;
+    pre.style.display = 'none' ;
+    
+}
+
+window.addEventListener('scroll' , () => {
+
+    let fixedNav= document.querySelector('.fixed-nav');
+
+    fixedNav.classList.toggle('active' , window.scrollY > 800);
+
+});
+
+
+    function tog() {
+        
+        let navbar = document.querySelector('.nav');
+        
+        navbar.classList.toggle('active');
+    }
+    
+    (()=> {
+    
+        let filterItems = document.querySelector('.filter');
+        let filterItem = document.querySelectorAll('.filter-item');
+        let gamesList = document.querySelectorAll('.games-list-ilem');
+
+
+        
+        
+        filterItems.addEventListener('click' , (event) => {
+            
+            
+            if (event.target.classList.contains('filter-item')  &&
+            !event.target.classList.contains('active')) {
+                
+                const see = event.target.getAttribute('data-target');
+
+
+            filterItems.querySelector('.filter-item.active').classList.remove('active');
+
+            event.target.classList.add('active');
+
+            gamesList.forEach((item) => {
+                
+                if (see === item.getAttribute('data-category')
+                || see === 'all') {
+                    item.classList.remove('hide');
+                    item.classList.add('show');
+                    console.log(item.getAttribute('data-category'))
+                }
+                else {
+                    item.classList.remove('show');
+                    item.classList.add('hide');
+
+                };
+            });
+
+
+            }
+
+
+        })
+    
+        })();
